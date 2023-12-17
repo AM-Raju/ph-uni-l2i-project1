d4fa0ab121d6e85efa4d39fc71cbc7e7c1350241
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-12-17 19:08:49 +0600

    Module 17.12 done

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -7,6 +7,7 @@ import { AcademicFacultyModel } from '../academicFaculty/academicFaculty.model';
 import { AcademicDepartmentModel } from '../academicDepartment/academicDepartment.model';
 import { CourseModel } from '../course/course.model';
 import { Faculty } from '../faculty/faculty.model';
+import { hasTimeConflict } from './offeredCourse.utils';
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
   const {
@@ -102,19 +103,12 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
 
   const newSchedule = { days, startTime, endTime };
 
-  assignedSchedules.forEach((schedule) => {
-    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
-    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
-    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
-    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
-
-    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
-      throw new AppError(
-        httpStatus.CONFLICT,
-        `This faculty isn't available at that time. Choose other time or day`,
-      );
-    }
-  });
+  if (hasTimeConflict(assignedSchedules, newSchedule)) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      `This faculty isn't available at that time. Choose other time or day`,
+    );
+  }
 
   const result = await OfferedCourseModel.create({
     ...payload,
