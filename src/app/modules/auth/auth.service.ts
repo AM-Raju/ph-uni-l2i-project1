d781f6f42360af6ca20bb54cfdd0cf6a6f4a8609
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-12-18 20:31:30 +0600

    Module 18.2 done

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,32 @@
+import httpStatus from 'http-status';
+import AppError from '../../error/AppError';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 
 const loginUser = async (payload: TLoginUser) => {
-  console.log(payload);
+  // Checking if the user is exist
+
+  const isUserExists = await User.findOne({ id: payload?.id });
+
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "This user isn't found");
+  }
+
+  // Checking if the user is already deleted
+  const isDeleted = isUserExists?.isDeleted;
+
+  if (!isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is Deleted');
+  }
+
+  // Checking if the user is blocked
+  const userStatus = isUserExists?.status;
+
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is Blocked');
+  }
+
+  // Access granted: Send AccessToken, RefreshToken
   return {};
 };
 
