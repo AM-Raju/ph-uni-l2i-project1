d0e380047361563bfec3c612de35659160876409
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-12-22 07:39:16 +0600

    Module 18.7 done

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -3,9 +3,10 @@ import AppError from '../error/AppError';
 import catchAsync from '../utils/catchAsync';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+import { TUserRole } from '../modules/user/user.interface';
 
 // auth HOC as middleware
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req, res, next) => {
     // console.log(req.headers.authorization);
     const token = req.headers.authorization;
@@ -22,6 +23,12 @@ const auth = () => {
       function (err, decoded) {
         // err
         if (err) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+        }
+
+        const role = (decoded as JwtPayload)?.role;
+
+        if (requiredRoles && !requiredRoles.includes(role)) {
           throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
         }
 
---
+++ src/app/modules/user/user.constant.ts
@@ -0,0 +1,5 @@
+export const USER_ROLE = {
+  student: 'student',
+  faculty: 'faculty',
+  admin: 'admin',
+} as const; // we used as const here to make it readonly
