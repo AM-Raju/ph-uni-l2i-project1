c6fb43c9050b83057cecdd929cb9ebf319216159
AM-Raju <mdmorshed.sj2005@gmail.com>
2023-12-31 11:32:12 +0600

    Module 20.2 done

--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,4 +1,16 @@
-const createEnrolledCourseIntoDB = async () => {};
+import { TEnrolledCourse } from './enrolledCourse.interface';
+
+const createEnrolledCourseIntoDB = async (
+  userId: string,
+  payload: TEnrolledCourse,
+) => {
+  /* 
+Business logic to create offeredCourse
+* Step 1: Check if the offered course exist.
+* Step 2: Check if the student is already enrolled.
+* Step 3: Create an enrolled course
+*/
+};
 
 export const EnrolledCourseServices = {
   createEnrolledCourseIntoDB,
